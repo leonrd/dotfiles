329359ac078cc2e825f2f0b2ca56b0570152d1b8
Leonard Chioveanu <leonard.chioveanu@sparktech.ro>
2019-10-16 17:07:17 +0300

    My own brew.sh

--- brew.sh
+++ brew.sh
@@ -1,9 +1,131 @@
 #!/usr/bin/env bash
 
+brews=(
+  # Install GNU core utilities (those that come with macOS are outdated).
+  # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+  coreutils
+  # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+  findutils
+  # Install GNU `sed`, overwriting the built-in `sed`.
+  gnu-sed --with-default-names
+  # Install a modern version of Bash.
+  bash
+  bash-completion2
+  # Install a modern shell.
+  zsh
+  zsh-completions
+  # Install `wget` with IRI support.
+  wget --with-iri
+  # Install GnuPG to enable PGP-signing commits.
+  gnupg
+  # Install more recent versions of some macOS tools.
+  git
+  grep
+  gmp
+  openssh
+  screen
+  python
+  vim --with-override-system-vi
+  neovim
+  # ruby <- install via rbenv
+  # Install font tools.
+  woff2
+  # Install monitoring tools
+  dfc
+  htop
+  # Install other useful binaries.
+  git-lfs
+  git-flow
+  imagemagick --with-webp
+  imagemagick@6
+  libimobiledevice
+  # lua
+  # luajit
+  pkg-config
+  readline
+  rlwrap
+  ssh-copy-id
+  # Install programming related tools
+  maven
+  nvm
+  postgresql
+  rbenv
+  sqlite
+  typescript
+  watchman
+  yarn
+  # switchjdk
+  # Install Brew Cask for GUI apps
+  caskroom/cask/brew-cask
+)
+
+casks=(
+  android-studio
+  bettertouchtool
+  brave
+  # cakebrew
+  # ccleaner
+  # crashlytics
+  # filezilla
+  # genymotion
+  google-chrome
+  iterm2
+  java
+  java6
+  # kodi
+  launchrocket
+  ngrok
+  # openemu
+  pgadmin3
+  # processing
+  # racket
+  sketch
+  skitch
+  # skype
+  sourcetree
+  # spectacle
+  # sqlitestudio
+  sublime-text3
+  # teamviewer
+  the-unarchiver
+  # transmission
+  virtualbox
+  visual-studio-code
+  vlc
+  # xquartz
+)
+
+pips=(
+  # Glances
+)
+
+gems=(
+  cocoapods
+  # travis
+  rails
+)
+
+npms=(
+)
+
+######################################## End of app list ########################################
+set +e
+
+# Install XCode command-line tools.
+echo "Installing Xcode Command Line Tools..."
+xcode-select --install
+
 # Install command-line tools using Homebrew.
 
-# Make sure we’re using the latest Homebrew.
-brew update
+ # Make sure we’re using the latest Homebrew.
+if test ! $(which brew); then
+  echo "Installing Homebrew ..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Updating Homebrew ..."
+  brew update
+fi
+brew doctor
 
 # Upgrade any already-installed formulae.
 brew upgrade
@@ -11,20 +133,32 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+fails=()
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
+function error {
+  red='\x1B[0;31m'
+  NC='\x1B[0m' # no color
+  msg="${red}Failed to execute: $1 $2${NC}"
+  fails+=($2)
+  echo -e $msg
+}
+
+function install {
+  cmd=$1
+  shift
+  for pkg in $@;
+  do
+    exec="$cmd $pkg"
+    echo "Executing: $exec"
+    if $exec ; then
+      echo "Installed $pkg"
+    else
+      error $cmd $pkg
+    fi
+  done
+}
+
+install 'brew install' ${brews[@]}
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
@@ -32,70 +166,22 @@ if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+install 'brew cask install --appdir='$HOME'/Applications' ${casks[@]}
+install 'pip install' ${pips[@]}
+install 'gem install' ${gems[@]}
+install 'npm install -g' ${npms[@]}
 
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
+echo "Upgrading ..."
+pip install --upgrade setuptools
+pip install --upgrade pip
+gem update --system
 
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-brew install php
-brew install gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries.
-brew install ack
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
-
-# Remove outdated versions from the cellar.
+echo "Cleaning up ..."
 brew cleanup
+brew cask cleanup
+brew linkapps
+
+for fail in ${fails[@]}
+do
+  echo "Failed to install: $fail"
+done
