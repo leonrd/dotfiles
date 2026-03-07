da8d7c5a3a59f3891bf69e48ad4729e1445108b4
leonrd <leonard.chioveanu@gmail.com>
2026-03-08 01:30:06 +0200

    .util: refactor pkg-install.sh

--- .util/macos/pkg-install.sh
+++ .util/macos/pkg-install.sh
@@ -10,7 +10,7 @@ trap cleanup SIGINT SIGTERM ERR EXIT
 cleanup() {
 	trap - SIGINT SIGTERM ERR EXIT
 
-  ${__dir}/pkg-cleanup.sh
+  "${__dir}"/pkg-cleanup.sh
 }
 
 # Ask for the administrator password upfront
@@ -23,49 +23,51 @@ while true; do
 	kill -0 "$$" || exit
 done 2>/dev/null &
 
-# Install XCode command-line tools.
-echo "Installing Xcode Command Line Tools..."
+echo "Installing Xcode Command Line Tools"
 xcode-select --install
-
-# Install command-line tools using Homebrew.
 
  # Make sure we’re using the latest Homebrew.
 if test ! $(which brew); then
-  echo "Installing Homebrew ..."
+  echo "Installing Homebrew"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-  echo "Updating Homebrew ..."
+  echo "Updating Homebrew"
   brew update
 fi
 
+echo "Running brew doctor"
 brew doctor
 
-echo "Upgrading outdated formulae..."
+echo "Upgrading outdated formulae"
 brew upgrade --greedy
 
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
+echo "Installing packages from Brewfile"
 brew bundle install
 
 if which brew &> /dev/null && [ -x $(brew --prefix)/bin/zsh ]; then
   case $- in
-    *i*) SHELL=$(brew --prefix)/bin/zsh; export SHELL; exec $SHELL -l;;
+    *i*)
+      echo "Setting brew provided zsh as SHELL"
+      SHELL=$(brew --prefix)/bin/zsh; export SHELL; exec $SHELL -l
+      ;;
   esac
 fi
 
-# Install ohmyzsh
+echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-mv ~/.zshrc ~/.zshrc.post-oh-my-zsh
-mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+mv "${HOME}/.zshrc" "${HOME}/.zshrc.post-oh-my-zsh"
+mv "${HOME}/.zshrc.pre-oh-my-zsh" "${HOME}/.zshrc"
 
-# Install zsh plugins
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+echo "Installing ohmyzsh plugins"
+git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-${ZSH:-${HOME}/.oh-my-zsh}/custom}/plugins/zsh-completions"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/Aloxaf/fzf-tab "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/fzf-tab"
 
-echo "Setting brew zsh as default shell"
 if ! fgrep -q "${BREW_PREFIX}/bin/zsh" /etc/shells; then
+  echo "Setting brew provided zsh as default shell"
   echo "${BREW_PREFIX}/bin/zsh" | sudo tee -a /etc/shells;
   chsh -s "${BREW_PREFIX}/bin/zsh";
 fi;
@@ -73,18 +75,39 @@ fi;
 # Fix oh-my-zsh warnings
 compaudit | xargs chmod g-w
 
+echo "Installing rbenv"
+git clone https://github.com/rbenv/rbenv.git "${HOME}/.rbenv"
+
+echo "Installing uv"
 curl -fsSL https://astral.sh/uv/install.sh | sh
 uv self update
 
-NPM_PREFIX=${NPM_PREFIX:-"$HOME/.npm-global"} \
-N_PREFIX=${NPM_PREFIX:-"$HOME/.n"} \
-	mkdir -p $NPM_PREFIX \
-	&& npm config set prefix "$NPM_PREFIX" \
-	&& npm install -g n \
-	&& n lts \
-	&& npm install -g yarn
+echo "Installing node lts via n script"
+curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s install lts
 
-# Install Kitty
+echo "Setting npm prefix"
+npm config set prefix "${NPM_PREFIX}"
+
+echo "Installing n"
+npm install -g n
+
+echo "Installing node lts via n"
+n lts
+
+echo "Installing yarn"
+npm install -g yarn
+
+echo "Installing kitty"
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+# Create a symbolic link to add kitty to PATH (assuming ${HOME}/.local/bin is in
+# your system-wide PATH)
+ln -s "${HOME}/.local/kitty.app/bin/kitty ${HOME}/.local/bin/"
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp "${HOME}/.local/kitty.app/share/applications/kitty.desktop ${HOME}/.local/share/applications/"
+# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+cp "${HOME}/.local/kitty.app/share/applications/kitty-open.desktop ${HOME}/.local/share/applications/"
+# Update the paths to the kitty and its icon in the kitty.desktop file(s)
+sed -i "s|Icon=kitty|Icon=/home/${USER}/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png"|g "${HOME}/.local/share/applications"/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=/home/${USER}/.local/kitty.app/bin/kitty"|g "${HOME}/.local/share/applications"/kitty*.desktop
 
 cleanup
--- .util/ubuntu/pkg-install.sh
+++ .util/ubuntu/pkg-install.sh
@@ -26,83 +26,94 @@ done 2>/dev/null &
 
 sudo apt-get update -y
 
+# Install basic tools
 sudo apt-get install -y --no-install-recommends \
 	man-db less sed curl wget git vim nano make bash tmux unzip gnupg gnupg2
 
+# Install awesome tools
+sudo apt-get install -y --no-install-recommends \
+	zsh fzf jq ripgrep bat git-flow htop dfc sqlite3 imagemagick ffmpeg sox \
+	screen rename rlwrap tree watchman
+
+# Install extra tools
+sudo apt-get install -y --no-install-recommends \
+	gh
+
+# Install git-delta
+GIT_DELTA_VERSION=0.18.2 \
+ARCH=$(dpkg --print-architecture) \
+	wget -q -O /tmp/git-delta.deb \
+		"https://github.com/dandavison/delta/releases/download/${GIT_DELTA_VERSION}/git-delta_${GIT_DELTA_VERSION}_${ARCH}.deb" \
+	&& sudo dpkg -i /tmp/git-delta.deb \
+	&& rm -f /tmp/git-delta.deb
+
+# Install ssh
 sudo apt-get install -y --no-install-recommends \
 	ssh \
 	&& sudo systemctl start ssh \
 	&& sudo systemctl enable ssh
 
-sudo apt-get install -y --no-install-recommends \
-	zsh fzf jq ripgrep bat git-flow htop dfc sqlite3 imagemagick ffmpeg sox \
-	screen rename rlwrap tree watchman
-
-GIT_DELTA_VERSION=0.18.2 
-ARCH=$(dpkg --print-architecture)
-wget -q -O /tmp/git-delta.deb \
-	"https://github.com/dandavison/delta/releases/download/${GIT_DELTA_VERSION}/git-delta_${GIT_DELTA_VERSION}_${ARCH}.deb" \
-&& sudo dpkg -i /tmp/git-delta.deb \
-&& rm /tmp/git-delta.deb
-
-# Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-mv $HOME/.zshrc $HOME/.zshrc.post-oh-my-zsh
-mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
-
-# Install zsh plugins
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab
-
-# Make zsh the default shell
-chsh -s $(which zsh)
-
-sudo apt-get install -y --no-install-recommends rbenv
-
-# curl https://pyenv.run | bash
-
-curl -fsSL https://astral.sh/uv/install.sh | sh
-uv self update
-
-sudo apt-get install -y --no-install-recommends nodejs
-NPM_PREFIX=${NPM_PREFIX:-"$HOME/.npm-global"} \
-N_PREFIX=${NPM_PREFIX:-"$HOME/.n"} \
-	mkdir -p $NPM_PREFIX \
-	&& npm config set prefix "$NPM_PREFIX" \
-	&& npm install -g n \
-	&& n lts \
-	&& npm install -g yarn
-
+# Install Docker
+echo "Installing Docker"
 sudo apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common \
 	&& curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg \
 	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
 	&& sudo apt-get update \
 	&& apt-cache policy docker-ce \
 	&& sudo apt-get install -y --no-install-recommends docker-ce \
-	&& sudo usermod -aG docker ${USER}
+	&& sudo usermod -aG docker "${USER}"
 sudo apt-get install -y --no-install-recommends docker-compose-plugin
+
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+mv "${HOME}/.zshrc" "${HOME}/.zshrc.post-oh-my-zsh"
+mv "${HOME}/.zshrc.pre-oh-my-zsh" "${HOME}/.zshrc"
+
+echo "Installing zsh plugins"
+git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-${ZSH:-${HOME}/.oh-my-zsh}/custom}/plugins/zsh-completions"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/Aloxaf/fzf-tab "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/fzf-tab"
+
+echo "Making zsh the default shell"
+chsh -s $(which zsh)
+
+echo "Installing rbenv"
+git clone https://github.com/rbenv/rbenv.git "${HOME}/.rbenv"
+
+echo "Installing uv"
+curl -fsSL https://astral.sh/uv/install.sh | sh
+uv self update
+
+echo "Installing node lts via n script"
+curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s install lts
+
+echo "Setting npm prefix"
+npm config set prefix "${NPM_PREFIX}"
+
+echo "Installing n"
+npm install -g n
+
+echo "Installing node lts via n"
+n lts
+
+echo "Installing yarn"
+npm install -g yarn
 
 # Install Kitty
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-# Create a symbolic link to add kitty to PATH (assuming $HOME/.local/bin is in
+# Create a symbolic link to add kitty to PATH (assuming ${HOME}/.local/bin is in
 # your system-wide PATH)
-ln -s $HOME/.local/kitty.app/bin/kitty $HOME/.local/bin/
+ln -s "${HOME}/.local/kitty.app/bin/kitty ${HOME}/.local/bin/"
 # Place the kitty.desktop file somewhere it can be found by the OS
-cp $HOME/.local/kitty.app/share/applications/kitty.desktop $HOME/.local/share/applications/
+cp "${HOME}/.local/kitty.app/share/applications/kitty.desktop ${HOME}/.local/share/applications/"
 # If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
-cp $HOME/.local/kitty.app/share/applications/kitty-open.desktop $HOME/.local/share/applications/
+cp "${HOME}/.local/kitty.app/share/applications/kitty-open.desktop ${HOME}/.local/share/applications/"
 # Update the paths to the kitty and its icon in the kitty.desktop file(s)
-sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" $HOME/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" $HOME/.local/share/applications/kitty*.desktop
+sed -i "s|Icon=kitty|Icon=/home/${USER}/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png"|g "${HOME}/.local/share/applications"/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=/home/${USER}/.local/kitty.app/bin/kitty"|g "${HOME}/.local/share/applications"/kitty*.desktop
 
 # Make kitty the default terminal emulator
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator `which kitty` 50
-
-wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
-echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" \
-    | sudo tee /etc/apt/sources.list.d/openresty.list
-sudo apt-get update && sudo apt-get install -y --no-install-recommends openresty
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg \
 	&& echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list \
@@ -128,5 +139,11 @@ sudo apt-get install -y --no-install-recommends dbeaver-ce
 sudo apt-get install -y --no-install-recommends openjdk-11-jdk
 sudo add-apt-repository -y ppa:maarten-fonville/android-studio
 sudo apt-get update && sudo apt-get install -y --no-install-recommends android-studio
+
+# Install OpenResty
+wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
+echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" \
+    | sudo tee /etc/apt/sources.list.d/openresty.list
+sudo apt-get update && sudo apt-get install -y --no-install-recommends openresty
 
 cleanup
