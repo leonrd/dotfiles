381721feece651b5c1838d318f70bfc1c5602c97
leonrd <leonard.chioveanu@gmail.com>
2026-03-07 18:24:11 +0200

    add .config/dotfiles/pull script to update the current installation

---
+++ .config/dotfiles/pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+__dir="$(cd "$(dirname "$0")" && pwd)"
+
+DOTFILES_HOME_DIR="$(cd "${__dir}/../../" && pwd)"
+
+function dotfiles {
+  git --git-dir=$DOTFILES_HOME_DIR/.dotfiles.git/ --work-tree=$DOTFILES_HOME_DIR $@
+}
+
+dotfiles pull
