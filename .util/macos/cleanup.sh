4354be355eb1c1413b441bdbe404490922c453cf
leonrd <leonard.chioveanu@gmail.com>
2026-03-07 21:14:14 +0200

    .util: remove useless color

--- .util/macos/cleanup.sh
+++ .util/macos/cleanup.sh
@@ -15,7 +15,7 @@ cleanup() {
 
 usage() {
 	cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-d] [--dry-run] [--no-color]
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-d] [--dry-run]
 
 A Mac Cleanup up Utility based on
 https://github.com/fwartner/mac-cleanup
@@ -25,18 +25,8 @@ Available options:
 -h, --help       Print this help and exit
 -v, --verbose    Print script debug info
 -d, --dry-run    Dry run
---no-color    	 Disable colors
 EOF
 	exit
-}
-
-# shellcheck disable=SC2034  # Unused variables left for readability
-setup_colors() {
-	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-		NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
-	else
-		NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
-	fi
 }
 
 msg() {
@@ -58,7 +48,6 @@ parse_params() {
 		-h | --help) usage ;;
 		-v | --verbose) set -x ;;
 		-d | --dry-run) dry_run=true ;;
-		--no-color) NO_COLOR=1 ;;
 		-?*) die "Unknown option: $1" ;;
 		*) break ;;
 		esac
@@ -69,7 +58,6 @@ parse_params() {
 }
 
 parse_params "$@"
-setup_colors
 
 deleteCaches() {
 	local cacheName=$1
